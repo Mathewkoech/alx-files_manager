a3e11b1c668a077e0ce6344165be7506328fd701
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-10-02 09:30:12 +0300

    index

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -116,3 +116,5 @@ class FileController {
     return null;
   }
 }
+
+export default FileController
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import AppController from "../controllers/AppController";
+import FileController from "../controllers/FilesController";
 
 const router = Router();
 
@@ -8,5 +9,10 @@ router.get("/status", async (req, res) => AppController.getStatus(req, res));
 router.get("/stats", async (req, res) => AppController.getStats(req, res));
 
 router.post("/files", async (req, res) => FileController.postUpload(req, res));
+
+router.post("/files/:id", async (req, res) => FileController.getShow(req, res))
+
+router.post("/files/", async (req, res) => FileController.getIndex(req, res))
+
 
 export default router;
